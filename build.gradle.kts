12318c788d28c0cc798aa01102280f28204231a2
idricealy <idricemourtadhoi@gmail.com>
2024-03-26 07:24:01 +0100

    feat: add spring security and update gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,11 @@ repositories {
 }
 
 dependencies {
+	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
